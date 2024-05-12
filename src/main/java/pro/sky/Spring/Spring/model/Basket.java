f14958fab8e7c56abd2a58cbc7806ffc3a02d626
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-05-12 21:32:37 +0800

    Подключение @SessionScope

--- src/main/java/pro/sky/Spring/Spring/model/Basket.java
+++ src/main/java/pro/sky/Spring/Spring/model/Basket.java
@@ -1,11 +1,13 @@
 package pro.sky.Spring.Spring.model;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 @Component
+@SessionScope
 public class Basket {
     private final List<Integer> productIds;
     public Basket() {
